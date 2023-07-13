7321a9e83864d650028b0cf42dc168cd3fc8ab6d
Majid <majidak347@gmail.com>
2023-07-13 21:14:19 +0530

    Added send email and new pages

--- dental/settings.py
+++ dental/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import Config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +128,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = Config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = Config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
--- website/views.py
+++ website/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
 
 def contact(request):
-    return render(request, 'contact.html', {})
+    if request.method == 'POST':
+        
+        message_name = request.POST['message_name']
+        message_email = request.POST['message_email']
+        message = request.POST['message']
+        return render(request, 'contact.html', {'message_name': message_name })
+    
+        # Send email
+        send_mail(
+            message_name,#subject
+            message,#message
+            message_email,#from
+            ['EMAIL_HOST_USER'],# to mail
+            fail_silently=False,
+        )
+
+    else:
+        # return the page
+        return render(request, 'contact.html', {})
